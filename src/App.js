42e963f01019eee86f9313025fc9865b39ba126e
Colin O'Sullivan <osullc2@rpi.edu>
2023-09-29 17:32:01 -0400

    Web description

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 
 function App() {
+
   return (
     <div className="EastieUnits">
       <header className="header">
         <h1>EastieUnits</h1>
+        <p1>EastieUnits is a website designated for the use of property managers and tenants/potential tenants, 
+          specifically in the Boston area, to act as a liaison between them.</p1>
       </header>
     </div>
   );
