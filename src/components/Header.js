70b1b9636e9e5bdf401d1f7637fb183479c82fe8
AndrewBodmer <bodmea2@rpi.edu>
2023-10-10 17:39:26 -0400

    Edited navbar and title

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,19 +3,26 @@ import "./Header.css"
 function Header() {
     return(
         <>
-            <nav className="navbar">
-                <ul className="navbar-list">
-                <li className="navbar-item">
-                    <a href="/">Home</a>
-                </li>
-                <li className="navbar-item">
-                    <a href="/search">Search</a>
-                </li>
-                <li className="navbar-item">
-                    <a href="/contact">Contact Info</a>
-                </li>
-                </ul>
-            </nav>
+            <div className="header">
+                <div className = "title">
+                    EastieUnits
+                </div>
+
+                <nav className="navbar">
+                    <ul className="navbar-list">
+                    <li className="navbar-item">
+                        <a href="/">Home</a>
+                    </li>
+                    <li className="navbar-item">
+                        <a href="/search">Search</a>
+                    </li>
+                    <li className="navbar-item">
+                        <a href="/contact">Contact Info</a>
+                    </li>
+                    </ul>
+                </nav>
+
+            </div>
         </>
     )
 }
