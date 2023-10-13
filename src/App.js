c044ba8b26942e0a0a5d1737f7d65b2ecf9456d2
AndrewBodmer <bodmea2@rpi.edu>
2023-10-13 16:56:20 -0400

    Added contact info section (fake info)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Contact from "./components/Contact"
 import Header from "./components/Header"
 
 function App() {
@@ -7,8 +8,13 @@ function App() {
     
     <div className="EastieUnits">
       <Header />
+
+
+      
+
       <p1>EastieUnits is a website designated for the use of property managers and tenants/potential tenants, 
           specifically in the Boston area, to act as a liaison between them.</p1>
+      <Contact />
     </div>
 
   );
