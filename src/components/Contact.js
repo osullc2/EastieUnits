de6192e8b5a1d108db1082799a50993205b95beb
AndrewBodmer <bodmea2@rpi.edu>
2023-10-13 17:27:26 -0400

    editted the contact information section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,12 +4,13 @@ function Contact(){
     return(
     <>
     <div className="contact-text">
+        <p1 style={{color:'white', textDecoration: 'underline', fontWeight: 'bold'}}>Contact Info</p1>
         <br />
-        <p1 style={{color:'white'}}>John Smith</p1>
+        <p2 style={{color:'white'}}>John Smith</p2>
         <br />
-        <p2 style={{color:'white'}}>123-456-7890</p2>
+        <p3 style={{color:'white'}}>(123) 456-7890</p3>
         <br />
-        <p3 style={{color:'white'}}>SmithJ.biz@email.com</p3>
+        <p4 style={{color:'white'}}>SmithJ.biz@email.com</p4>
     </div>
     </>
     )
