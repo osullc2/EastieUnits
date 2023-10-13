3cd72b165de5fbad0277c88d0a0ae0e056d39a3e
Colin O'Sullivan <osullc2@rpi.edu>
2023-10-13 16:59:45 -0400

    Reorganized main app

--- src/App.js
+++ src/App.js
@@ -10,10 +10,6 @@ function App() {
     <div className="EastieUnits">
       <Header />
       <Desc />
-
-
-      <p1>EastieUnits is a website designated for the use of property managers and tenants/potential tenants, 
-          specifically in the Boston area, to act as a liaison between them.</p1>
       <Contact />
     </div>
 
