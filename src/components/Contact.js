4780dbe98121f17f078b52529aad2444e21d994e
Colin O'Sullivan <osullc2@rpi.edu>
2023-10-13 17:12:34 -0400

    Played around with contact info div

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,20 +3,14 @@ import "./Contact.css"
 function Contact(){
     return(
     <>
-        <div className="contact-text">
+    <div className="contact-text">
         <br />
-        <p1>
-            John Smith
-        </p1>
+        <p1 style={{color:'white'}}>John Smith</p1>
         <br />
-        <p2>
-            123-456-7890
-        </p2>
+        <p2 style={{color:'white'}}>123-456-7890</p2>
         <br />
-        <p3>
-            SmithJ.biz@email.com
-        </p3>
-        </div>
+        <p3 style={{color:'white'}}>SmithJ.biz@email.com</p3>
+    </div>
     </>
     )
 }
