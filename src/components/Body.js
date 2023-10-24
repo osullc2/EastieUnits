3e9bf527df5aa5d13984c5845d5ef14886a4f076
Colin O'Sullivan <osullc2@rpi.edu>
2023-10-24 18:51:22 -0400

    Progress on Tile components, changed CSS/layout

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,31 @@
-import "./Body.css"
-import Tile from "./Tile.js"
+import React from "react";
+import "./Body.css";
+import Tile from "./Tile.js";
+import tile1 from "./newTiles/tile1.jpg";
+import tile2 from "./newTiles/tile2.jpg";
+import tile3 from "./newTiles/tile3.jpg";
+import tile4 from "./newTiles/tile4.jpg";
+import tile5 from "./newTiles/tile5.jpg";
 
 function Body() {
+    const tileData = [
+        { img: tile1, desc: "house1" },
+        { img: tile2, desc: "house2" },
+        { img: tile3, desc: "house3" },
+        { img: tile4, desc: "house4" },
+        { img: tile5, desc: "house5" },
+    ];
+
     return (
-    <>
-        <Tile img="./newTiles/tile1.jpg" desc="This is a house"/>
-
-
-    </>
-    )
+        <div className="tiles">
+            {tileData.map((tile, index) => (
+                <React.Fragment key={index}>
+                    <Tile img={tile.img} desc={tile.desc} />
+                    {index % 3 === 2 && <div className="row-divider" />}
+                </React.Fragment>
+            ))}
+        </div>
+    );
 }
 
-export default Body
+export default Body;
--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,18 +1,18 @@
 import "./Tile.css"
 
+
 // I want to give this component the img src as a prop, but since this react project uses webpack,
 // we need to use the require() function which pretty much ruins everything. Will need to restructure
 // to make the project independant of webpack and stuff.
 function Tile(props) {
     return (
         <>
-            <img src={require("./newTiles/tile1.jpg")} alt="Not Found"/>
-            <h2> {props.desc} </h2>
-        
-        
-
-
-        
+        <div className="tile">
+            <div className="content">
+                <img src={props.img} alt="Not Found"/>
+                <h2> {props.desc} </h2>
+            </div>     
+        </div>
         </>
     )
 }
