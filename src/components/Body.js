f3d4781090784538407c96f244b594573c56e834
Colin O'Sullivan <osullc2@rpi.edu>
2023-10-27 17:29:01 -0400

    Tile formatting

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,6 +6,11 @@ import tile2 from "./newTiles/tile2.jpg";
 import tile3 from "./newTiles/tile3.jpg";
 import tile4 from "./newTiles/tile4.jpg";
 import tile5 from "./newTiles/tile5.jpg";
+import tile6 from "./newTiles/tile6.jpg";
+import tile7 from "./newTiles/tile7.jpg";
+import tile8 from "./newTiles/tile8.jpg";
+import tile9 from "./newTiles/tile9.jpg";
+import tile10 from "./newTiles/tile10.jpg";
 
 function Body() {
     const tileData = [
@@ -14,6 +19,11 @@ function Body() {
         { img: tile3, desc: "house3" },
         { img: tile4, desc: "house4" },
         { img: tile5, desc: "house5" },
+        { img: tile6, desc: "house6" },
+        { img: tile7, desc: "house7" },
+        { img: tile8, desc: "house8" },
+        { img: tile9, desc: "house9" },
+        { img: tile10, desc: "house10" },
     ];
 
     return (
