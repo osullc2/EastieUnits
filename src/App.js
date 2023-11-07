428d9f29f35a634f8e4b75249dc468d4cec16bf7
Colin O'Sullivan <osullc2@rpi.edu>
2023-11-07 17:21:33 -0500

    Created first separate webpage using React Router

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import './App.css';
 import Header from "./components/Header"
 import Body from "./components/Body"
 import Footer from "./components/Footer"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Tile1 from "./components/Routes/Tile1"
 
 function App() {
 
   return (
-    
-    <div className="EastieUnits">
-      <Header />
-      <Body />
-      <Footer />
-    </div>
+    <Router>
+      <div className="EastieUnits">
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Body />} />
+            <Route path="/Tile1" element={<Tile1 />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
 
   );
 }
---
+++ src/components/Routes/Tile1.js
@@ -0,0 +1,10 @@
+function Tile1() {
+    return (
+        <>
+            <h1> This is the page for tile 1</h1>
+            <p1> Here is some info</p1>
+        </>
+    )
+}
+
+export default Tile1
