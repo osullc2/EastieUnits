9e20f6c20356bf95e58f7ed15cb7302818317ee1
Colin O'Sullivan <osullc2@rpi.edu>
2023-11-13 10:10:40 -0500

    Small comment

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -3,7 +3,7 @@ import "./Tile.css"
 function Tile(props) {
     return (
         <>
-        <a href="/Tile1">
+        <a href="/Tile1"/* Replace this with props.route, where the route passed in is /Tile1, /Tile2, /Tile3, etc*/> 
             <div className="tile">
                 <div className="content">
                     <img src={props.img} alt="Not Found"/>
